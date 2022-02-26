8c919cb2d74e390893fe7a0783873cbce51fd86e
Marlon <gmrmarlon17@gmail.com>
2022-02-26 16:17:56 -0400

    adicionando links

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
- <div class="bg-slate-600 p-6">
-    <h1 class="text-2xl text-white text-center">{{ $route.name }}</h1>
+  <div class="bg-slate-600 p-6 flex justify-between">
+    <h1 class="text-2xl text-white">{{ $route.name }}</h1>
+    <div>
+      <router-link class="text-white mx-2" to="/home">home</router-link>
+      <router-link class="text-white mx-2" to="/funcionarios">funcionarios</router-link>
+    </div>
   </div>
   <router-view></router-view>
 </template>
@@ -8,5 +12,4 @@
 
 
 <style>
-
 </style>
